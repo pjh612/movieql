a6f32347d52231219f43497265dac4c8b8b06c89
pjh612 <62292492+pjh612@users.noreply.github.com>
2021-01-04 00:32:04 +0900

    Movie API with graphQl

---
+++ db.js
@@ -0,0 +1,35 @@
+import fetch from "node-fetch";
+const API_URL = "https://yts.am/api/v2/list_movies.json?";
+
+export const getSuggestions = async (id) => {
+  const movie = await fetch(
+    `https://yts.mx/api/v2/movie_suggestions.json?movie_id=${id}`
+  )
+    .then((res) => res.json())
+    .then((json) => json.data.movie);
+
+  return movie;
+};
+
+export const getMovie = async (id) => {
+  const movie = await fetch(
+    `https://yts.am/api/v2/movie_details.json?movie_id=${id}`
+  )
+    .then((res) => res.json())
+    .then((json) => json.data.movie);
+  //console.log(movie);
+  return movie;
+};
+
+export const getMovies = (limit, rating) => {
+  let REQUEST_URL = API_URL;
+  if (limit > 0) {
+    REQUEST_URL += `limit=${limit}`;
+  }
+  if (rating > 0) {
+    REQUEST_URL += `&minimum_rating=${rating}`;
+  }
+  return fetch(REQUEST_URL)
+    .then((res) => res.json())
+    .then((json) => json.data.movies);
+};
---
+++ graphql/resolvers.js
@@ -0,0 +1,9 @@
+import { getMovie, getMovies, getSuggestions } from "../db.js";
+const resolvers = {
+  Query: {
+    movies: (_, { rating, limit }) => getMovies(limit, rating),
+    movie: (_, { id }) => getMovie(id),
+    suggestions: (_, { id }) => getSuggestions(id),
+  },
+};
+export default resolvers;
